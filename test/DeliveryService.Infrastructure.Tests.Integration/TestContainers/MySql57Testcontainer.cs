dc3a321737918a1a3d49f9017920f36e21fd1494
kashifsoofi <kashif.soofi+git@gmail.com>
2021-02-09 21:44:16 +0000

    Update Testcontainer version

---
+++ test/DeliveryService.Infrastructure.Tests.Integration/TestContainers/MySql57Testcontainer.cs
@@ -0,0 +1,14 @@
+﻿namespace DeliveryService.Infrastructure.Tests.Integration.Testcontainers
+{
+    using DotNet.Testcontainers.Containers.Configurations;
+    using DotNet.Testcontainers.Containers.Modules.Abstractions;
+
+    public class MySql57Testcontainer : TestcontainerDatabase
+    {
+        internal MySql57Testcontainer(ITestcontainersConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public override string ConnectionString => $"Server={this.Hostname};Port={this.Port};Database={this.Database};Uid={this.Username};Pwd={this.Password};";
+    }
+}
